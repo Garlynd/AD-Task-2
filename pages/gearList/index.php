cb33572bc58d4aa80d9dae6f4696761cd79daf0d
Garlynd <weinard21@gmail.com>
2025-05-17 16:06:48 +0800

    feat(gearList): update title and add CSS links for styling

--- pages/gearList/index.php
+++ pages/gearList/index.php
@@ -8,7 +8,9 @@ $gears = getGearData();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gear List</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/gearList.css">
 </head>
 <body>
     
