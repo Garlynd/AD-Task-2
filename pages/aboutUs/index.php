6a81b33690439d6cafedde0cbb0d6e93331bf923
Garlynd <weinard21@gmail.com>
2025-05-17 17:55:13 +0800

    fix(aboutUs): correct stylesheet path for about us page

--- pages/aboutUs/index.php
+++ pages/aboutUs/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Road Cyclers</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/about.css">
+    <link rel="stylesheet" href="../../pages/aboutUs/assets/css/style.css">
 </head>
 <body>
     
