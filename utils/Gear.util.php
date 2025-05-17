a0de5b234cd426578a9c2b870965247c802810cf
Garlynd <weinard21@gmail.com>
2025-05-17 23:04:42 +0800

    fix(gear): update prices to reflect correct currency values

--- utils/Gear.util.php
+++ utils/Gear.util.php
@@ -5,19 +5,19 @@ function getGearData() {
         [
             'name' => 'Specialized Tarmac SL7',
             'type' => 'Road Bike',
-            'price' => '$5,000',
+            'price' => '₱280,000',
             'image' => 'assets/img/tarmac.jpg'
         ],
         [
             'name' => 'Shimano Ultegra R8100',
             'type' => 'Groupset',
-            'price' => '$1,500',
+            'price' => '₱85,000',
             'image' => 'assets/img/ultegra.jpg'
         ],
         [
             'name' => 'Kask Protone Helmet',
             'type' => 'Helmet',
-            'price' => '$300',
+            'price' => '₱17,000',
             'image' => 'assets/img/helmet.jpg'
         ]
     ];
