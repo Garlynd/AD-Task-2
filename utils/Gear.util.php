9a2384a3f9871e72f2e07f0e80f1e7506d93cb5c
Garlynd <weinard21@gmail.com>
2025-05-17 16:10:26 +0800

    feat(Gear.util): implement gear data retrieval and welcome message functions

---
+++ utils/Gear.util.php
@@ -0,0 +1,28 @@
+<?php
+
+function getGearData() {
+    return [
+        [
+            'name' => 'Specialized Tarmac SL7',
+            'type' => 'Road Bike',
+            'price' => '$5,000',
+            'image' => '../../assets/img/tarmac.jpg'
+        ],
+        [
+            'name' => 'Shimano Ultegra R8100',
+            'type' => 'Groupset',
+            'price' => '$1,500',
+            'image' => '../../assets/img/ultegra.jpg'
+        ],
+        [
+            'name' => 'Kask Protone Helmet',
+            'type' => 'Helmet',
+            'price' => '$300',
+            'image' => '../../assets/img/helmet.jpg'
+        ]
+    ];
+}
+
+function getWelcomeMessage() {
+    return "Explore the Road with Premium Cycling Gear!";
+}
