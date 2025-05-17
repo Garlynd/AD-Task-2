17cd8a6c6efc4e749603ae0e0e22a18a20438d6e
Garlynd <weinard21@gmail.com>
2025-05-17 15:58:37 +0800

    refactor(landing page): remove PHP includes and restructure index.php to a basic HTML template

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php
-require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
-
-require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
-require_once BASE_PATH . '/components/templates/foot.component.php';
-require_once BASE_PATH . '/components/templates/head.component.php';
-require_once BASE_PATH . '/components/templates/nav.component.php';
-?>
-
-<?php
-head('Product List');
-navHeader($navList);
-?>
-
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
-</section>
-
-<?php
-footer();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
