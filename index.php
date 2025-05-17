5007047b1a9907dc3726055aab866d42c3f89155
Garlynd <weinard21@gmail.com>
2025-05-17 16:49:02 +0800

    fix(landing page): add missing stylesheet link in the head section

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $welcomeMessage = getWelcomeMessage();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cycling Gear</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <?php include 'components/nav.component.php'; ?>
