74fce086a2f1016ee20638c88b14604f83a954b5
Garlynd <weinard21@gmail.com>
2025-05-17 16:12:58 +0800

    feat(gearCard): add gear card component template

---
+++ components/templates/gearCard.component.php
@@ -0,0 +1,6 @@
+<div class="gear-card">
+    <img src="<?php echo $gear['image']; ?>" alt="<?php echo $gear['name']; ?>">
+    <h3><?php echo $gear['name']; ?></h3>
+    <p>Type: <?php echo $gear['type']; ?></p>
+    <p>Price: <?php echo $gear['price']; ?></p>
+</div>
