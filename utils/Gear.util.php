943c281fa34d8768fc7561881ea28658231624b4
Garlynd <weinard21@gmail.com>
2025-05-17 22:50:10 +0800

    fix(gear): update image paths to use relative paths correctly

--- utils/Gear.util.php
+++ utils/Gear.util.php
@@ -6,19 +6,19 @@ function getGearData() {
             'name' => 'Specialized Tarmac SL7',
             'type' => 'Road Bike',
             'price' => '$5,000',
-            'image' => '../../assets/img/tarmac.jpg'
+            'image' => 'assets/img/tarmac.jpg'
         ],
         [
             'name' => 'Shimano Ultegra R8100',
             'type' => 'Groupset',
             'price' => '$1,500',
-            'image' => '../../assets/img/ultegra.jpg'
+            'image' => 'assets/img/ultegra.jpg'
         ],
         [
             'name' => 'Kask Protone Helmet',
             'type' => 'Helmet',
             'price' => '$300',
-            'image' => '../../assets/img/helmet.jpg'
+            'image' => 'assets/img/helmet.jpg'
         ]
     ];
 }
