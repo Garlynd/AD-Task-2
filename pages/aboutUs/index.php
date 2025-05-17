4405687dd5aef42d6488802fdbf550e5207ebc74
Garlynd <weinard21@gmail.com>
2025-05-17 17:50:20 +0800

    feat(aboutUs): add about us page with styles

---
+++ pages/aboutUs/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../../utils/Gear.util.php';
+$gears = getGearData();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gear List</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../pages/gearList/assets/css/style.css">
+</head>
+<body>
+    <?php include '../../components/nav.component.php'; ?>
+    <main>
+        <h1>Road Cycling Gear</h1>
+        <div class="gear-list">
+            <?php foreach ($gears as $gear): ?>
+                <?php include '../../components/templates/gearCard.component.php'; ?>
+            <?php endforeach; ?>
+        </div>
+        <a href="../../index.php" class="button">← Back to Home</a>
+    </main>
+    <script src="../../assets/js/script.js"></script>
+    <script src="assets/js/gearList.js"></script>
+</body>
+</html>
