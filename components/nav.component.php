3b514684b8f12e55d363989754f7f20615daa5d6
Garlynd <weinard21@gmail.com>
2025-05-27 16:06:15 +0800

    fix(nav): update navigation links to use relative paths for better portability

--- components/nav.component.php
+++ components/nav.component.php
@@ -1,6 +1,6 @@
 <nav>
-    <a href="/AD-Task-2/index.php">Home</a> |
-    <a href="/AD-Task-2/pages/gearList/index.php">Gear List</a> |
-    <a href="/AD-Task-2/pages/aboutUs/index.php">About Us</a>
+    <a href="/../index.php">Home</a> |
+    <a href="/../pages/gearList/index.php">Gear List</a> |
+    <a href="/../pages/aboutUs/index.php">About Us</a>
 </nav>
 <hr>
